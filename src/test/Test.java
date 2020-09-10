3dd4abed8bde4bde8fb8a7db4d4634af00cd6f01
vuhe <zhuhe202@qq.com>
2020-09-10 16:31:42 +0800

    200910-4

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,16 +3,34 @@ package test;
 
 // 类名
 public class Test {
+    private final static int N = 20000;// 素数范围
+    private static final boolean[] arrPrimeTable = new boolean[N + 1];// 筛选求素数
+
     // 主函数
     public static void main(String[] args) {
-        int number1 = 12;// 定义第一个变量
-        int number2 = 21;// 定义第二个变量
-        System.out.println("交换前：number1 = " + number1 + ", number2 = " + number2);
-        int tmp = number1;
-        number1 = number2;
-        number2 = tmp;
-        System.out.println("交换后：number1 = " + number1 + ", number2 = " + number2);
-        char ch = '\uabcd';
-        System.out.println(ch);
+        //初始化
+        for (int i = 2; i <= N; i++) {
+            arrPrimeTable[i] = true;
+        }
+        //进行剔除
+        for (int i = 2; i * i <= N; i++) {
+            if (arrPrimeTable[i]) {
+                //剔除i的倍数：t = x * i
+                for (int t = i + i; t <= N; t += i) {
+                    arrPrimeTable[t] = false;
+                }
+            }
+        }
+        // 输出
+        int count = 0;
+        for (int i = 2; i <= N; i++) {
+            if (arrPrimeTable[i]) {
+                System.out.print(i + "\t");
+                count++;
+                if (count % 15 == 0) {
+                    System.out.print("\n");
+                }
+            }
+        }
     }
 }
