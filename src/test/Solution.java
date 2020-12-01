94ad5fb7bab8723f5bc8542975f24ab326ab5892
vuhe <zhuhe202@qq.com>
2020-12-01 11:06:06 +0800

    feat: 201201-1

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -8,56 +8,27 @@ import java.util.*;
 
 class Solution {
 
-    public ListNode insertionSortList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        for (int i = 0; i < A.length / 2; i++) {
+            int temp = A[i];
+            A[i] = A[A.length - i - 1];
+            A[A.length - i - 1] = temp;
         }
-        ListNode newList = new ListNode(0);
-        newList.next = head;
-        ListNode oldList = new ListNode(0);
-        oldList.next = head.next;
-        head.next = null;
-        while (oldList.next != null) {
-            ListNode ptr = oldList.next;
-            oldList.next = ptr.next;
-            ListNode p = newList;
-            while (p.next != null && p.next.val < ptr.val) {
-                p = p.next;
+        for (int i = 0; i < A.length - 2; i++) {
+            for (int j = i + 1; j + 1 < A.length; j++) {
+                if (A[i] < A[j] + A[j + 1]) {
+                    return A[i] + A[j] + A[j + 1];
+                }
             }
-            ptr.next = p.next;
-            p.next = ptr;
         }
-        return newList.next;
+        return 0;
     }
 
-    /**
-     * 给你一个数组 nums和一个值 val，你需要 原地 移除所有数值等于val的元素，并返回移除后数组的新长度。
-     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
-     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
-     */
-    public int removeElement(int[] nums, long val) {
-        if (val < Integer.MIN_VALUE || Integer.MAX_VALUE < val) {
-            throw new RuntimeException("val 输入错误！");
-        }
-        if (nums == null) {
-            throw new RuntimeException("nums 不能为 null！");
-        }
-        int size = nums.length;
-        for (int i = 0; i < size; i++) {
-            while (i < size && nums[i] == val) {
-                int t = nums[size -1];
-                nums[size -1] = nums[i];
-                nums[i] = t;
-                size--;
-            }
-        }
-        return size;
-    }
-    
     public static void main(String... args) {
         Solution s0 = new Solution();
 //        System.out.println(Arrays.toString(new int[]{}));
-        s0.removeElement(null, 1);
+//        s0.removeElement(null, 1);
 //        double key = s0.maxProbability(3, new int[][]{{0,1},{1,2},{0,2}}, new double[]{0.5,0.5,0.2}, 0, 2);
 //        System.out.println(key);
 //        Map<Integer, Integer> a = new HashMap<>();
