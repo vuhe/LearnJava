713a2d60ca7455ce44039706826f2f7e75bf9a62
vuhe <zhuhe202@qq.com>
2020-12-11 16:07:20 +0800

    feat: 201211-2

--- src/test/Main.java
+++ src/test/Main.java
@@ -6,8 +6,16 @@ import java.io.File;
  * @author vuhe
  */
 public class Main {
+    /**
+     * 驱动程序
+     * <p>
+     * 用于拉起整个程序的测试运行
+     *
+     * @param args 入参
+     */
     public static void main(String... args) {
-        Triangle test = SystemIo.readFileToTriangle(new File(""));
-        SystemIo.printTriangle(test);
+//        SystemIo.writeTriangleToFile(Triangle.of(2, 2, 3));
+        var f = SystemIo.readFileToTriangle(new File("./testcase/triangle.txt"));
+        SystemIo.printTriangle(f);
     }
 }
--- src/test/SystemIo.java
+++ src/test/SystemIo.java
@@ -1,12 +1,22 @@
 package test;
 
-import java.io.File;
+import java.io.*;
+import java.util.Objects;
 
 /**
  * @author vuhe
  */
 public class SystemIo {
+    /**
+     * 默认存储路径
+     */
+    private static final String PATH = "./testcase/triangle.txt";
 
+    /**
+     * 打印三角形
+     *
+     * @param triangle 三角形
+     */
     public static void printTriangle(Triangle triangle) {
         System.out.print("此三角形边三长为 ");
         System.out.print(triangle.a + " ");
@@ -19,13 +29,53 @@ public class SystemIo {
         System.out.println("\t三角形周长为 " + triangle.getPerimeter());
     }
 
+    /**
+     * 将三角形写入文件
+     *
+     * @param triangle 三角形
+     * @return 写入的文件
+     */
     public static File writeTriangleToFile(Triangle triangle) {
-        return new File("./testcase");
+        File file = new File(PATH);
+        try (var out = new FileWriter(file)) {
+            out.write(triangle.a + "\n");
+            out.write(triangle.b + "\n");
+            out.write(triangle.c + "\n");
+            out.write(Objects.hash(triangle.a, triangle.b, triangle.c) + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
     }
 
+    /**
+     * 从文件读取三角形
+     *
+     * @param file 文件
+     * @return 三角形
+     */
     public static Triangle readFileToTriangle(File file) {
-        return Triangle.of(2, 2, 3);
+        // 检查是否是文件
+        if (!file.isFile()) {
+            throw new IllegalArgumentException("传入文件错误");
+        }
+        // 生产的三角形
+        Triangle triangle = null;
+        // 读取
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            int[] n = new int[3];
+            for (int i = 0; i < 3; i++) {
+                n[i] = Integer.parseInt(br.readLine());
+            }
+            // hash值校验
+            if (Objects.hash(n[0], n[1], n[2]) != Integer.parseInt(br.readLine())) {
+                throw new IOException("文件损坏，检查文件");
+            }
+            triangle = Triangle.of(n[0], n[1], n[2]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 返回
+        return triangle;
     }
-
-
 }
